f60f7b4f9fb669a25667aee1cb10ea578e46b936
Ryan Lott <toranlyt@gmail.com>
2018-11-22 13:31:44 -0600

    Added interest finder logic

--- script.py
+++ script.py
@@ -26,9 +26,24 @@ add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
 add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
 add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
 add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
-add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
-add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Sao Paulo, Brazil", ["Sao Paulo Zoo", ["zoo"]])
+add_attraction("Sao Paulo, Brazil", ["Patio do Colegio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
 
-test_destination_index = get_traveler_location(test_traveler)
+def find_attractions(destination, interests):
+  destination_index = get_destination_index(destination)
+  attractions_in_city = attractions[destination_index]
+  attractions_with_interest = []
+  
+  for possible_attraction in attractions_in_city:
+    attraction_tags = possible_attraction[1]
+    for interest in interests:
+      if interest in attraction_tags:
+        attractions_with_interest.append(possible_attraction[0])
+  return attractions_with_interest
+
+la_arts = find_attractions("Los Angeles, USA", ['art'])
+
+print(la_arts)
+  
